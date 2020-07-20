1d6aac069068e71aae56a83fc6a46b3a9fdbf36e
Jonathan Gabor <67616175+JonathanGabor@users.noreply.github.com>
2020-07-20 16:32:25 -0400

    SA files now concatenated in correct order

--- correctness_test/test_correctness.sh
+++ correctness_test/test_correctness.sh
@@ -1,5 +1,5 @@
 INPUT_FOLDER=$1
 SA_FOLDER=$2
 gcc -o test_sa utils.c test_sa.c
-cat ${SA_FOLDER}/suffixarray_* > final_sa
+cat $(find ${SA_FOLDER} -name "suffixarray*" | sort -V) > final_sa
 ./test_sa ${INPUT_FOLDER} final_sa
